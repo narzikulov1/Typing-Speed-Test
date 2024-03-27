dfb12eee07b1a6cc87a7abf72046c4c929e2bd6b
Sardor Narzikulov <sardornarzikulovv2002@gmail.com>
2024-03-27 13:06:24 +0500

    checking user typed

---
+++ script.js
@@ -0,0 +1,29 @@
+const typingText = document.querySelector(".typing-text p"),
+    inpField = document.querySelector(".wrapper .input-field");
+
+let charIndex = 0;
+
+function randomParagraph() {
+    let randIndex = Math.floor(Math.random() * paragraphs.length);
+    paragraphs[randIndex].split("").forEach(span => {
+        let spanTag = `<span>${span}</span>`;
+        typingText.innerHTML += spanTag;
+    });
+
+    document.addEventListener("keydown", () => inpField.focus());
+    typingText.addEventListener("click", () => inpField.focus());
+}
+
+function initTyping() {
+    const characters = typingText.querySelectorAll("span");
+    let typedChar = inpField.value.split("")[charIndex];
+    if (characters[charIndex].innerText === typedChar) {
+        characters[charIndex].classList.add("correct");
+    } else {
+        characters[charIndex].classList.add("incorrect");
+    }
+    charIndex++;
+}
+
+randomParagraph();
+inpField.addEventListener("input", initTyping);
