a45f1b3914528e210e4cb25ba14b81131970f6bd
Sardor Narzikulov <sardornarzikulovv2002@gmail.com>
2024-03-27 10:33:27 +0500

    paragraphs

---
+++ paragrapht.js
@@ -0,0 +1,17 @@
+
+const paragraphs = [
+    "I dont mind admitting that Im a simple sort of mouse I live inside a nice cosy log by the side of a field My needs are not great A few sunflower seeds or wheat" ,
+    "stalks will do me for a meal For a special treat the farmer sometimes leaves me some crumbs of bread and cheese from his lunch When I am thirsty I drink from the" ,
+    "bubbling stream I swear that pure cold water is the freshest most delicious taste in the world Finer than champagne even and I dont just mean that as a boast or" ,
+    "a figure of speech I tried champagne once so I know what Im talking about How did a poor simple country mouse like myself try champagne Well I shall tell you" ,
+    "A while back I received a visit from my uncle the town mouse Everyone in my family knows that Uncle Town Mouse is very rich and successful and lives in a big smart" ,
+    "house in the city It was of course an honour that he should come and stay with me for a weekend away from his business But to tell you the truth I felt a little bit" ,
+    "nervous What would he make of my humble abode and my simple tastes Well of course he was very nice and polite Charming simply charming he said as I showed him" ,
+    "around my log When I put some crumbs of cheddar cheese before him for his supper he exclaimed: Just the ticket Exactly what I wanted Thank you dear nephew for taking" ,
+    "such good care of me At night he slept in my spare bed in the hedgerow and in the morning when I asked him how he had slept he said: Splendid just splendidly This" ,
+    "clear country air of yours is so restful In fact he was so full of praise for my country lifestyle that I asked him if he was planning to retire to the countryside" ,
+    "Uncle Town Mouse laughed and I felt that I had perhaps said something silly or tactless My dear nephew he said The countryside is all very well for a rest But the" ,
+    "town is the place to live if you appreciate gourmet food fine wine and by the way the most stylish and elegant ladymice Why I thank you for your hospitality but" ,
+    "I would die of boredom if I lived your life for more than a weekend I was very impressed by my uncles words and I could not stop thinking about the attractions of" ,
+    "the city that he had described"
+]
